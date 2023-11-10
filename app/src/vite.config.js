0c9947c20cddfa36ad47f27be8ea193349bb9b99
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-11-10 23:38:21 +0800

    build done

--- app/src/vite.config.js
+++ app/src/vite.config.js
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import nodePolyfills from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills()
+  ],
   resolve: {
     alias: [{
       find: "src", replacement: resolve(__dirname, "src")
     }]
   },
   build: {
-		minify: 'esbuild',
-		target: "esnext"
-	},
+    minify: 'esbuild',
+    target: "esnext"
+  },
   define: {
     'process.env': {}
   }
-})
+});
